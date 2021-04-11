2143420275a02de96c3af4d1e5101e2c38de4e82
Kundan Kumar <rockingkundan26@gmail.com>
2021-04-11 21:12:45 +0530

    first commit

---
+++ springboot-backened/springboot-backened/src/test/java/com/example/springbootbackened/SpringbootBackenedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootbackened;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBackenedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
